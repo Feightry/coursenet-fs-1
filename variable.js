aafd6db8aa4aeb4833c17a6b02c5ee8ba1e38024
Gery Halim <gery.halim@gmail.com>
2022-04-16 15:26:28 +0700

    Adding variable.js

---
+++ variable.js
@@ -0,0 +1,26 @@
+/**Variable */
+
+let number = 20;
+let words = "testing variable"
+let isAvailable = true;
+
+console.log(typeof number);
+console.log(typeof words);
+console.log(typeof isAvailable);
+
+/** X % N = 0<Result<N */
+
+let x = 10;
+let y = '5';
+
+var z = x + y;
+var za = x - y;
+
+console.log(z);
+console.log(za);
+
+// NaN = Not a Number
+
+/** a === b
+ * to compare value a with b also the type of both
+ */
